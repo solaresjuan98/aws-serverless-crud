715ccc666b3065f1294318bee59e3780917ed198
Juan Antonio Solares <solaresjuan43@gmail.com>
2023-09-01 10:32:02 -0600

    Add getTask, updateTask, deleteTask and Middy

--- src/addTask.js
+++ src/addTask.js
@@ -1,10 +1,13 @@
 const { v4 } = require("uuid");
 const AWS = require("aws-sdk");
 
+const middy = require("@middy/core");
+const jsonBodyParser  = require("@middy/http-json-body-parser");
+
 const addTask = async (event) => {
   const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-  const { title, description } = JSON.parse(event.body);
+  const { title, description } = event.body;
   const createdAt = new Date();
   const id = v4();
 
@@ -16,19 +19,19 @@ const addTask = async (event) => {
     done: false,
   };
 
-  await dynamodb.put({
-    TableName: "TaskTable",
-    Item: newTask,
-  }).promise();
-
+  await dynamodb
+    .put({
+      TableName: "TaskTable",
+      Item: newTask,
+    })
+    .promise();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(newTask)
-  }
-
+    body: JSON.stringify(newTask),
+  };
 };
 
 module.exports = {
-  addTask,
+  addTask: middy(addTask).use(jsonBodyParser()),
 };
